5c06048b99d433e41dcae2d49711c95557ed50c6
Sykkoss <damien.cosson@epitech.eu>
2018-12-17 17:24:57 +0100

    Enum SFM done + beginning of game system

---
+++ Assets/Scripts/GameSystemBasic1v1.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class GameSystemBasic1v1 : MonoBehaviour {
+
+	EnumSFM.SFM P1SFM;
+	EnumSFM.SFM P2SFM;
+	int P1Won;
+	int P2Won;
+
+	// Rounds
+
+	private void Start()
+	{
+		P1Won = 0;
+		P2Won = 0;
+	}
+}
