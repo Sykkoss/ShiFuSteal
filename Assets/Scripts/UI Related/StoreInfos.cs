a2403c3d965aa3e36effb70ae2e2e2e014bd8dd1
Sykkoss <damien.cosson@epitech.eu>
2018-12-19 20:59:58 +0100

    Number of rounds can now be put in input box (Custom number of rounds)

--- Assets/Scripts/UI Related/StoreInfos.cs
+++ Assets/Scripts/UI Related/StoreInfos.cs
@@ -1,11 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StoreInfos : MonoBehaviour {
+
+	public Text inputText;
 
 	public void SetNumberRounds(int newNumber)
 	{
 		NbrLocalRounds.numberRoundsNeeded = newNumber;
+	}
+
+	public void SetCustomNumberRounds()
+	{
+		NbrLocalRounds.numberRoundsNeeded = int.Parse(inputText.text);
 	}
 }
