10eabb31d2301bed18349b0ab3cfaf09e66ff0e8
Sykkoss <damien.cosson@epitech.eu>
2019-01-14 11:05:00 +0100

    Beginning of abilities (with function pointer)

---
+++ Assets/Scripts/Abilities/MageAbilities.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MageAbilities : MonoBehaviour {
+
+	public delegate void AbilityPointer();
+
+	AbilityPointer[] ability_pointer;
+
+	void Start()
+	{
+		ability_pointer = new AbilityPointer[2];
+
+		ability_pointer[0] = Ability1;
+		ability_pointer[1] = Ability2;
+	}
+
+	public void FunctionPointer(int number)
+	{
+		ability_pointer[number]();
+	}
+
+	/* N°1 */
+	void Ability1()
+	{
+		print("Ability 1");
+	}
+
+	/* N°2 */
+	void Ability2()
+	{
+		print("Ability 2");
+	}
+}
---
+++ Assets/Scripts/GameSystem/NbrLocalRounds.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class NbrLocalRounds{
+
+	public static int numberRoundsNeeded = 3;
+
+	public static void SetNumberRoundsNeeded(int newNumber)
+	{
+		numberRoundsNeeded = newNumber;
+	}
+
+	public static int GetNumberRoundsNeeded()
+	{
+		return numberRoundsNeeded;
+	}
+}
